5fe7e3083217d924b987078cc2568b4c36488e09
Mohamed Yaser <mohamedyaser.315@gmail.com>
2025-07-06 22:33:30 +0300

    integrate to frontend the stats

--- script.js
+++ script.js
@@ -31,18 +31,20 @@ async function getStats() {
   if (response.status === 404) {
     document.getElementById("stats").innerHTML =
       "<p>Please register the URL first.</p>";
+    document.getElementById("charts").innerHTML = '';
   } else {
     const stats = await response.json();
+    let chartsHtml = '';
+    chartsHtml += `<div class='chart-block'><h4>Device Distribution</h4><img src='${stats.device_chart}' alt='Device Distribution' style='max-width:400px;max-height:400px;width:100%;margin:10px;'></div>`;
+    chartsHtml += `<div class='chart-block'><h4>OS Distribution</h4><img src='${stats.os_chart}' alt='OS Distribution' style='max-width:400px;max-height:400px;width:100%;margin:10px;'></div>`;
+    chartsHtml += `<div class='chart-block'><h4>Browser Distribution</h4><img src='${stats.browser_chart}' alt='Browser Distribution' style='max-width:400px;max-height:400px;width:100%;margin:10px;'></div>`;
+    chartsHtml += `<div class='chart-block'><h4>Hourly Distribution</h4><img src='${stats.hour_chart}' alt='Hourly Distribution' style='max-width:600px;max-height:400px;width:100%;margin:10px;'></div>`;
+    chartsHtml += `<div class='chart-block'><h4>Day Distribution</h4><img src='${stats.day_chart}' alt='Day Distribution' style='max-width:600px;max-height:400px;width:100%;margin:10px;'></div>`;
+    chartsHtml += `<div class='chart-block'><h4>Month Distribution</h4><img src='${stats.month_chart}' alt='Month Distribution' style='max-width:600px;max-height:400px;width:100%;margin:10px;'></div>`;
+    let busyHours = stats.busy_hours ? stats.busy_hours.join(", ") : "N/A";
     document.getElementById("stats").innerHTML = `
-            <p>Original URL: <a href=${stats.original_url}>${stats.original_url}</a></p>
-
-            <p>Shortened URL: <a href=http://localhost:8000/${stats.new_url}>http://localhost:8000/${stats.new_url}</a></p>
-
-            <p>Created At: 
-                <p class="inner-stat">${stats.craeted_at}</p>
-            </p>
-
-            <p>Clicks: <p class="inner-stat"> ${stats.clicks}</p></p>
-        `;
+      <p><b>Busy Hours:</b> <span class="inner-stat">${busyHours}</span></p>
+    `;
+    document.getElementById("charts").innerHTML = `<div class='charts-flex'>${chartsHtml}</div>`;
   }
 }
